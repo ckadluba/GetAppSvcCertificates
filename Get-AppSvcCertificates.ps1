22eb8d9f4fa4d8d5f8f05449354ce1a48a8a50e0
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-01-17 09:20:02 +0100

    Fixed handling of subscription (parameter was ignored if az context already set).

--- Get-AppSvcCertificates.ps1
+++ Get-AppSvcCertificates.ps1
@@ -71,7 +71,7 @@ function SearchAndLinkSllBindingsForWebApp {
 }
 
 $context = Get-AzContext
-if ($null -eq $context.Subscription || $context.Subscription.Name -ne $Subscription) {
+if (($null -eq $context.Subscription) -or ($context.Subscription.Name -ne $Subscription)) {
     Write-Host "Connect account and set context to subscription $Subscription"
     Connect-AzAccount -Subscription "$Subscription"
 }
